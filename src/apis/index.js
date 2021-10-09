f8069628676cd62e099c3d8e5bc58c9b533a05ce
Pranshu-Gupta <pranshugupta2910@gmail.com>
2021-10-09 23:24:38 +0530

    changed domain name of API

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://memories----project.herokuapp.com/posts';
+const url = 'https://memories----project-api.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 
