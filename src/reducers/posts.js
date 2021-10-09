69ccc78e6f0d7bf654e8ec97333f2aa9595fa3f5
Pranshu-Gupta <pranshugupta2910@gmail.com>
2021-10-09 23:14:33 +0530

    first commit

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,5 +1,5 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
-export default (posts = [], action) => {
+const postReducer = (posts = [], action) => {
   switch (action.type) {
     case DELETE:
       return posts.filter((post) => post._id !== action.payload);
@@ -15,3 +15,4 @@ export default (posts = [], action) => {
       return posts;
   }
 };
+export default postReducer;
